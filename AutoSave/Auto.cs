3a1fc6faaa8cae217e590e1404642a5cd92a0a9e
SmallGarfield <2946491890@qq.com>
2020-08-20 14:42:33 +0800

    v1.0 release

---
+++ AutoSave/Auto.cs
@@ -0,0 +1,53 @@
+﻿using Logger = Rocket.Core.Logging.Logger;
+using Rocket.Core.Plugins;
+using System;
+using Rocket.Unturned.Chat;
+using SDG.Unturned;
+
+namespace AutoSave
+{
+    public class AutoSave : RocketPlugin<AutoSaveConfiguration>
+    {
+        protected override void Load()
+        {
+            Logger.LogWarning("####################################");
+            Logger.LogWarning("#            AutoSave              #");
+            Logger.LogWarning("#          QQ:2946491890           #");
+            Logger.LogWarning("#          SmallGarfield           #");
+            Logger.LogWarning("####################################");
+            Instance = this;
+        }
+
+        protected override void Unload()
+        {
+        }
+
+        public void FixedUpdate()
+        {
+            AutoSaveEvent();
+        }
+
+        public static AutoSave Instance;
+        public DateTime start = DateTime.Now;
+        public bool msgYorN;
+        public void AutoSaveEvent()
+        {
+            if (!Configuration.Instance.Enabled)
+            {
+                return;
+            }
+            if (start != null && (DateTime.Now - start).TotalSeconds > Configuration.Instance.SaveInterval)
+            {
+                if (!msgYorN)
+                {
+                    UnturnedChat.Say(Configuration.Instance.SaveMsg);
+                    msgYorN = true;
+                }
+                SaveManager.save();
+                msgYorN = false;
+                start = DateTime.Now;
+                Logger.Log("Saved at " + DateTime.Now);
+            }
+        }
+    }
+}
---
+++ AutoSave/AutoSaveConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Rocket.API;
+namespace AutoSave
+{
+    public class AutoSaveConfiguration : IRocketPluginConfiguration
+    {
+        public bool Enabled;
+        public int SaveInterval;
+        public string SaveMsg;
+        public void LoadDefaults()
+        {
+            Enabled = true;
+            SaveInterval = 180;
+            SaveMsg = "服务器保存中...";
+        }
+    }
+}
